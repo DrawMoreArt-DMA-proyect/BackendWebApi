15bbceaa18eebb1dea77a90ec1d00f1e7ed7363a
alexuwu <kazawasaque@gmail.com>
2021-11-22 19:45:12 -0600

    Agregando Solucion

---
+++ mx.edu.uttt.dma.webapi/Entidades/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace mx.edu.uttt.dma.webapi.Entidades
+{
+    public class Usuario
+    {
+        [Key]
+        public int IdUsuario { get; set; }
+        [Required]
+        [StringLength(20)]
+        public string UsuarioNombre { get; set; }
+        [StringLength(50)]
+        public string CorreoElectronico { get; set; }
+        [StringLength(15)]
+        public string Contrasena { get; set; }
+    }
+}
